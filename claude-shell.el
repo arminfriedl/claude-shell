5efd5d6dae05468e784b5c630b5fcaa8855dd629
Armin Friedl <dev@friedl.net>
2024-07-07 19:02:59 +0200

    Update shell-maker to 0.50.5

--- claude-shell.el
+++ claude-shell.el
@@ -5,10 +5,10 @@
 ;; Author: Armin Friedl <dev@friedl.net>
 ;; Maintainer: Armin Friedl <dev@friedl.net>
 ;; Created: März 16, 2024
-;; Version: 0.0.5
+;; Version: 0.0.6
 ;; Keywords: anthropic claude claude-shell shell-maker terminals wp help tools
 ;; Homepage: https://github.com/arminfriedl/claude-shell
-;; Package-Requires: ((emacs "29.1") (shell-maker "0.50.1"))
+;; Package-Requires: ((emacs "29.1") (shell-maker "0.50.5"))
 ;;
 ;; This file is not part of GNU Emacs.
 
